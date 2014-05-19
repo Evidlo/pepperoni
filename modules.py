04e6028d1044f465b719db93ab4645a83ce99b91
evan <evan@diode.ecn.purdue.edu>
2014-05-19 01:14:43 -0400

    added youtube module

--- modules.py
+++ modules.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#Modules for pepperoni bot - Modules must begin with module_
 from twisted.internet import reactor
 from random import choice
 #youtube
@@ -8,7 +9,7 @@ import re
 #food
 from datetime import datetime
 
-class shesaid(object):
+class module_shesaid(object):
 	def __init__(self,config):
 		self.enabled = True
 		self.rate = int(config.get('shesaid','rate',0))
@@ -18,29 +19,32 @@ class shesaid(object):
 		with open(quotesFile) as quotesFileObj:
 			self.quotes = quotesFileObj.readlines()
 
-	def run(self,user,msg):
-		self.enabled = False
-		#schedule this module to be reenabled after 'self.rate' seconds
-		reactor.callLater(self.rate,lambda:self.enable())
-		return choice(self.quotes)
-
 	def enable(self):
 		self.enabled = True
 
+	def run(self,bot):
+		self.enabled = False
+		#schedule this module to be reenabled after 'self.rate' seconds
+		reactor.callLater(self.rate,lambda:self.enable())
+		bot.msg(bot.channel,choice(self.quotes))
 
-class youtube(object):
+
+class module_youtube(object):
 	def __init__(self,config):
 		self.enabled = True
 		self.rate = int(config.get('youtube','rate',0))
 		triggers = config.get('youtube','triggers')
 		self.triggers = triggers.split('\n')
 
-	def run(self,user,msg):
+	def enable(self):
+		self.enabled = True
+
+	def run(self,bot):
 		self.enabled = False
 		#schedule this module to be reenabled after 'self.rate' seconds
 		reactor.callLater(self.rate,lambda:self.enable())
 		
-		id = re.match(".*?v=([a-zA-Z0-9_-]{11}).*",str(msg))
+		id = re.match(".*?v=([a-zA-Z0-9_-]{11}).*",str(bot.chat))
 		if id:
 			id = id.group(1)
 			url = 'http://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2' % id 
@@ -59,11 +63,61 @@ class youtube(object):
 				ratings='0'
 			title = video['entry']['title']['$t']
 			views = video['entry']['yt$statistics']['viewCount']
-			return(u'▷ '.encode('utf-8')+ title +" - " + views +" views"+" - "+"Rating " + str(rating)+"% - "+ratings+" ratings"+u' ◁'.encode('utf-8'))
+			bot.msg(bot.channel,u'▷ '.encode('utf-8')+ title +" - " + views +" views"+" - "+"Rating " + str(rating)+"% - "+ratings+" ratings"+u' ◁'.encode('utf-8'))
+
+
+class module_food(object):
+	def __init__(self,config):
+		self.enabled = False
+		self.rate = int(config.get('food','rate',0))
+		triggers = config.get('food','triggers')
+		self.triggers = triggers.split('\n')
+		self.acceptable_courts = ["hillenbrand","ford","wiley","earhart","windsor"]
+		self.acceptable_mealtimes = acceptable_mealtime=["Lunch","Dinner","Breakfast"]
 
 	def enable(self):
-		self.enabled = True
+		self.enabled = False
+	
+	def foodHelp(bot):
+		bot.notice(bot.user," Usage: !food COURT [MEAL] [YYYY-MM-DD]")
+		bot.notice(bot.user," Example usage: !food hillenbrand")
+		bot.notice(bot.user," Example usage: !food hillenbrand lunch 2013-12-29")
+		return
 
+	def run(self,bot):
+		self.enabled = False
+		#schedule this module to be reenabled after 'self.rate' seconds
+		reactor.callLater(self.rate,lambda:self.enable())
+		
+		params = chat.split(' ')
 
+		if len(params) < 2 or 'help' in params or '-h' in params:
+			foodHelp(bot)
+			return
+
+		for param in params:
+			if param.lower() in self.acceptable_courts:
+				court = param.lower()
+			if param.title() in self.acceptable_mealtimes:
+				meal = param.title()
+			if re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}',param.lower()):
+				day = datetime.strptime(param,'%Y-%m-%d')
+		if not court:
+			foodHelp(bot)
+		if not meal:
+			time = datetime.now()
+			hour = time.strftime('%H')
+			if hour > 14:
+				meal = 'Dinner'
+			elif hour > 10:
+				meal = 'Lunch'
+			else:
+				meal = 'Breakfast' 
+
+			if not day:
+				day = time
+		else:
+			if not day:
+				foodHelp(bot)
 
 
