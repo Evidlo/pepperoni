d9f970df10ccd5c9e349ea1e493186add07b169e
Evan <evan@evanw.org>
2014-08-24 00:26:05 -0400

    All references to modules now have the 'module_' prefix Added !loaded command to see what modules are loaded Added blacklisting feature to blacklist certain commands on different irc servers Cleaned up code for loading modules and bot instances

--- pepperoni.py
+++ pepperoni.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/python
 
 #Evan Widloski - 2014-05-12 - evan@evanw.org
 #An IRC bot written in python-twisted
@@ -15,7 +15,6 @@ import modules
 
 class Bot(irc.IRCClient):
 
-
 	def _get_nickname(self):
 		return self.factory.nickname
 	nickname = property(_get_nickname)
@@ -27,12 +26,22 @@ class Bot(irc.IRCClient):
 		self.loadModules()
 
 	def loadModules(self):
+		logging.debug('Loading modules...')
+		logging.debug('Using blacklist: '+self.factory.blacklist.__repr__())
 		#reload module settings
-		config.read('settings.ini')		
-		#set up all the modules (ignore builtins)
-		myclasses = {}
-		execfile('modules.py',myclasses)
-		self.mymodules = {name:myclass(config,self) for name,myclass in myclasses.items() if name.startswith('module_')}
+		config.read('settings.ini')
+
+
+		self.modules = [] 
+		raw_modules = {}
+		execfile('modules.py',raw_modules)
+
+		#only load modules that start with 'module_' and aren't blacklisted for this server	
+		for name,module in raw_modules.items():
+			logging.debug('Checking module %s'%name)
+			if name.startswith('module_') and name not in self.factory.blacklist:
+				logging.debug('Loading module %s'%name)
+				self.modules.append(module(config,self))
 
 	def joined(self, channel):
 		logging.info("Joined %s." % channel)
@@ -50,7 +59,7 @@ class Bot(irc.IRCClient):
 		logging.debug("Private Message:"+chat)
 
 		#check message against triggers for every module
-		for module in self.mymodules.values():
+		for module in self.modules:
 			if module.enabled:
 				for trigger in module.triggers:
 					if re.search(trigger,chat):
@@ -63,9 +72,10 @@ class Bot(irc.IRCClient):
 class BotFactory(protocol.ClientFactory):
 	protocol = Bot
 
-	def __init__(self, channels, nickname):
+	def __init__(self, channels, nickname, blacklist):
 		self.channels = channels
 		self.nickname = nickname
+		self.blacklist = blacklist
 
 	def clientConnectionLost(self, connector, reason):
 		logging.info("Connection lost. Reason: %s" % reason)
@@ -74,14 +84,16 @@ class BotFactory(protocol.ClientFactory):
 	def clientConnectionFailed(self, connector, reason):
 		logging.info("Connection failed. Reason: %s" % reason)
 
+
+
 if __name__ == "__main__":
-		config = ConfigParser()
-		config.read('settings.ini')		
-		logging.basicConfig(filename='/tmp/peetsa.log',level=logging.DEBUG)
-		
-		#start up each instance of the bot, and join each channel/server with given nick
-		instances = zip(config.get('bot','host').split(','),config.get('bot','channel').split(','),config.get('bot','nick').split(','))
-		for instance in instances:
-			print instance
-			reactor.connectTCP(instance[0], 6667, BotFactory(instance[1].split(' '),instance[2]))
-		reactor.run()
+	config = ConfigParser()
+	config.read('settings.ini')		
+	logging.basicConfig(filename=config.get('bot','logfile'),level=logging.DEBUG)
+	
+	#start up each instance of the bot, and join each channel/server with given nick
+	instances = [{'host':host,'channels':channels,'nick':nick,'blacklist':blacklist} for host,channels,nick,blacklist in zip(config.get('bot','host').split(','),config.get('bot','channel').split(','),config.get('bot','nick').split(','),config.get('bot','blacklist').split(','))]
+	for instance in instances:
+		print instance
+		reactor.connectTCP(instance['host'], 6667, BotFactory(instance['channels'].split(' '),instance['nick'],instance['blacklist'].split(' ')))
+	reactor.run()
