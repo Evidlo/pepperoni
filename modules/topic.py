844989219cd367473fbacd87578ca991fc68c436
Evan <evan@evanw.org>
2014-09-11 07:49:01 -0400

    updated gcal api url to return correct results

--- modules/topic.py
+++ modules/topic.py
@@ -1,3 +1,4 @@
+#Evan Widloski - 2014-09-11
 from twisted.internet import reactor, defer
 from twisted.web.client import getPage
 from basemodules import botmodule
@@ -15,20 +16,24 @@ class module_topic(botmodule):
 		#schedule this module to be reenabled after 'self.rate' seconds
 		reactor.callLater(self.rate,lambda:self.enable())
 		date = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S-0500')
-		url = 'https://www.googleapis.com/calendar/v3/calendars/0q5kmi03heskp39fpg73iniapc%40group.calendar.google.com/events?maxResults=2&timeMin='+date+'&fields=items%28end%2Cstart%2Csummary%2Clocation%29&key='+self.key
+		url = 'https://www.googleapis.com/calendar/v3/calendars/0q5kmi03heskp39fpg73iniapc%40group.calendar.google.com/events?orderBy=startTime&singleEvents=true&maxResults=2&timeMin='+date+'&fields=items%28start%2Csummary%2Clocation%29&key='+self.key
 		self.log.info('Using key:'+self.key)
+
+		#grab data from api
 		try:
 			data = json.load(urllib.urlopen(url))
 		except:
 			self.log.info('Unable to fetch Gcal results')
 
-		message = 'Upcoming Events :: '
+		events = []
 		for event in data['items']:
 			#read in date, chop off 6 char timezone
 			time = datetime.datetime.strptime(event['start']['dateTime'][:-6],'%Y-%m-%dT%H:%M:%S')
-			
-			message+='%s - %s at %s | '%(event['summary'],time.strftime('%a., %b. %d, %I:%M%p'),event['location'])
+			#apply custom formatting to data
+			events.append('{0} - {1} at {2}'.format(event['summary'],time.strftime('%a., %b. %d, %I:%M%p'),event['location']))
 
+		message = 'Upcoming Events :: ' + ' | '.join(events)
 		self.log.info('Got calendar results')
 		self.log.info(message)
 		self.bot.topic(self.bot.channel,message.encode('utf8'))
+		self.bot.msg('test')
