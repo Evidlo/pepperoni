e71e94002f302cfb0d5dc04025fca562f7339305
Evan Widloski <evan@evanw.org>
2015-07-31 23:29:04 -0600

    updated urbandictionary api call

--- modules/udict.py
+++ modules/udict.py
@@ -5,18 +5,20 @@ from basemodules import botmodule
 
 #urban dictionary definition grabber
 class module_poodict(botmodule):
-	def run(self):
-		args='%20'.join(self.bot.chat.split(' '))
-		if len(args) >= 2:
-			word = args[1:]
-			url = "http://api.urbandictionary.com/v0/define?term="+word
-			json = simplejson.load(urllib.urlopen(url))
-			getPage(url).addCallback(simplejson.loads).addCallback(results)
-
-	def results(self,json):
-		try:
-			definition = json['list'][0]['definition']
-			message = ':: ' + definition.split('.')[0]
-			self.bot.msg(self.bot.channel,message)
-		except KeyError:
-			self.log.info('Definition not found')
+    def results(self,json):
+            self.log.info('Retrieving from udict')
+            self.log.info(json)
+            try:
+                    definition = json['list'][0]['definition']
+                    message = ':: ' + definition[:10] + definition[10:].split('.')[0]
+                    self.bot.msg(self.bot.channel,message)
+            except KeyError:
+                    self.log.info('Definition not found')
+    def run(self):
+            args=self.bot.chat.split(' ')
+            if len(args) >= 2:
+                    word='%20'.join(args[1:])
+                    url = "http://api.urbandictionary.com/v0/define?term="+word
+                    self.log.info(url)
+                    json = simplejson.load(urllib.urlopen(url))
+                    getPage(url).addCallback(simplejson.loads).addCallback(self.results)
