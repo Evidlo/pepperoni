735f71dd7e9a3b7f4d4a4c2188a545c7273a9418
evan <evan@evanw.org>
2016-10-20 20:08:16 -0400

    added translate

--- modules/dict.py
+++ modules/dict.py
@@ -5,14 +5,29 @@ import simplejson
 
 #get regular definitions from glosbe dictionary
 class module_dict(botmodule):
-	def results(self,json):
-		definition = json['tuc'][0]['meanings'][0]['text']
-		self.bot.msg(self.bot.channel,(':: ' + definition[:25]+definition[25:].split('.')[0])[:150])
+    def results_dictionary(self,json):
+        definition = json['tuc'][0]['meanings'][0]['text']
+        self.bot.msg(self.bot.channel,(':: ' + definition[:25]+definition[25:].split('.')[0])[:150])
+    def results_translate(self,json):
+        definition = json['tuc'][0]['phrase']['text']
+        self.bot.msg(self.bot.channel,(':: ' + definition[:25]+definition[25:].split('.')[0])[:150])
 
-	def run(self):
-		definition = ''
-		args=self.bot.chat.split(' ')
-		if len(args) >= 2:
-			url='http://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase=' + args[1]
-			getPage(url).addCallback(simplejson.loads).addCallback(self.results)
+    def run(self):
+        definition = ''
+        args=self.bot.chat.split(' ')
+        if len(args) >= 2:
+            if args[0] == '!dict':
+                self.log.debug('Definition lookup requested')
+                url='http://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase=' + args[1]
+                self.log.debug('Getting url: '+url)
+                getPage(url).addCallback(simplejson.loads).addCallback(self.results_dictionary)
+            if args[0] == '!trans':
+                if len(args) >= 4:
+                    self.log.debug('Translation requested')
+                    from_lang = args[1]
+                    to_lang = args[2]
+                    phrase = args[3]
+                    url='http://glosbe.com/gapi/translate?from={0}&dest={1}&format=json&phrase={2}'.format(from_lang,to_lang,phrase)
+                    self.log.debug('Getting url: '+url)
+                    getPage(url).addCallback(simplejson.loads).addCallback(self.results_translate)
 
