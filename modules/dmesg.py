3385dd6a8ac732406021ee6d13c3072537270d32
evan <evan@evanw.org>
2016-12-22 17:05:05 -0500

    add ping info to dmesg.py

--- modules/dmesg.py
+++ modules/dmesg.py
@@ -4,5 +4,12 @@ from basemodules import botmodule
 
 #dmesg
 class module_dmesg(botmodule):
-	def run(self):
-		self.bot.msg(self.bot.channel,'boggle...')
+    def pong(self,user=None,latency=None):
+        self.log.debug('Pong called with user={0}, time={1}'.format(user,latency))
+        self.bot.msg(self.bot.channel, 'Round trip latency to {0}: {1}s'.format(user, latency))
+
+    def run(self):
+        self.log.debug('pinging...')
+        self.bot.ping('Evidlo','fart')
+        self.bot.append_ping_queue('Evidlo', self.pong)
+        self.bot.msg(self.bot.channel,'boggle...')
