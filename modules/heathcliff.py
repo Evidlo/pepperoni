a7845b1934af87f0eac1e272f2665a08207bcb36
evan <evan@evanw.org>
2017-01-12 22:30:38 -0500

    update heathcliff xpath

--- modules/heathcliff.py
+++ modules/heathcliff.py
@@ -11,5 +11,5 @@ class module_heathcliff(botmodule):
 		opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0')]
 		response = opener.open('http://www.gocomics.com/heathcliff')
 		tree = etree.HTML(response.read())
-		url = tree.xpath('//img[@class="strip"]')[0].attrib['src']
+        url = tree.xpath('//picture[@class="img-fluid item-comic-image"]/img')[0].attrib['src'] 
 		self.bot.msg(self.bot.channel,url)
