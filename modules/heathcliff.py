bece05c5a82c4b98f2569391de4de99ac6db0537
evan <evan@evanw.org>
2017-01-12 22:32:24 -0500

    fix heathcliff tabs

--- modules/heathcliff.py
+++ modules/heathcliff.py
@@ -5,11 +5,11 @@ import urllib2
 
 #heathcliff
 class module_heathcliff(botmodule):
-	def run(self):
-		from lxml import etree
-		opener = urllib2.build_opener()
-		opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0')]
-		response = opener.open('http://www.gocomics.com/heathcliff')
-		tree = etree.HTML(response.read())
-        url = tree.xpath('//picture[@class="img-fluid item-comic-image"]/img')[0].attrib['src'] 
-		self.bot.msg(self.bot.channel,url)
+    def run(self):
+        from lxml import etree
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0')]
+        response = opener.open('http://www.gocomics.com/heathcliff')
+        tree = etree.HTML(response.read())
+        url = tree.xpath('//picture[@class="img-fluid item-comic-image"]/img')[0].attrib['src']
+        self.bot.msg(self.bot.channel,url)
