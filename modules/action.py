7946640762935f778e363aaeef982df32ef761e8
evan <evan@evanw.org>
2017-08-05 00:31:53 -0400

    add missing setting

--- modules/action.py
+++ modules/action.py
@@ -5,10 +5,11 @@ from basemodules import botmodule
 # bot responds to certain uses of action command
 # e.g. `/me slaps pepperoni` makes pepperoni slap the person back
 class module_action(botmodule):
-	def run(self):
+    def run(self):
         args = self.bot.chat.split(' ')
-        if len(args) >= 1:
-			message = args.plit(' ')[0]+' '+self.bot.user
-			self.bot.msg(self.bot.channel,'\001ACTION %s\001' % message)
-		except IndexError:
-			pass
+        try:
+            if len(args) >= 1:
+                message = args.split(' ')[0]+' '+self.bot.user
+                self.bot.msg(self.bot.channel,'\001ACTION %s\001' % message)
+        except IndexError:
+            pass
